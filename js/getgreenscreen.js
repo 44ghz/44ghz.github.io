ad8b46fd3a31d12410eb6fc06cf4c6c94e014c9d
George Perez <44957922+44ghz@users.noreply.github.com>
2019-06-24 14:07:23 -0400

    Fixed issue with highlighting
    Fixed issue where code would sometimes not be highlighted

--- js/getgreenscreen.js
+++ js/getgreenscreen.js
@@ -4,17 +4,30 @@ function get_content()
 	var content = responseObj.content;
 	content = atob(content); // Decoding the content of the file
 
-	hljs.initHighlightingOnLoad();
+	//hljs.initHighlightingOnLoad();
 	// Creating element for code display
-	$(document.body).append("<div class='wrapper'><pre class='pre-scrollable'><code class='hljs java'>" + content + "</code></pre></div>");
-	hljs.initLineNumbersOnLoad();
+	$(document.body).append('<div class="wrapper" style="padding-left: 300px; padding-right: 300px">' +
+	'<div class="custom-control custom-switch" style="padding-left: 43px">' +
+	'	<input type="checkbox" class="custom-control-input" id="numberSwitch" checked>' +
+	'	<label class="custom-control-label" for="numberSwitch" style="user-select: none">Toggle line numbers</label>' +
+	'</div><pre><code class="hljs java">' + content + '</code></pre></div>');
 
 	//hljs.configure({ useBR: true });
-	// document.addEventListener('DOMContentLoaded', (event) => {
-	// 	document.querySelectorAll('pre code').forEach((block) => {
-	// 		hljs.highlightBlock(block);
-	// 	});
-	// });
+	//hljs.initLineNumbersOnLoad();
+	document.querySelectorAll('pre code').forEach((block) => {
+			hljs.highlightBlock(block);
+		});
+
+	$(document).ready(function() {
+		$('code.hljs').each(function(i, block) {
+			hljs.lineNumbersBlock(block);
+		});
+	});
+
+	var script = document.createElement("script");
+	script.src = "js/lineswitch.js";
+
+	document.head.appendChild(script);
 }
 
 function send_request()
