83dffb23bf6e1b61796ace4c4ece5be694cd873c
George Perez <44957922+44ghz@users.noreply.github.com>
2019-07-05 15:46:25 -0400

    Fixed a bug with using nightmode for the first time

--- js/nightmode.js
+++ js/nightmode.js
@@ -2,33 +2,7 @@ function toggleNightMode()
 {
 	var page = document.getElementsByTagName("html")[0]; // Get the entire html page as an object
 
-	if(localStorage["mode"] === "light") // If the site is in light mode, make it dark
-	{
-		page.style.cssText = "--mydark: #ced0d9";
-		page.style.cssText += "--mylight: #282c34";
-		page.style.cssText += "--mygray: #1f2329";
-		page.style.cssText += "--mylight-gray: #343b45"
-		page.style.cssText += "--mydark-gray: #21242b";
-		page.style.cssText += "--nav-hover: #3b3645";
-		page.style.cssText += "--nav-gradient-1: #33303d";
-		page.style.cssText += "--nav-gradient-2: #1c1e24";
-		page.style.cssText += "--display-light: #1a191f"
-		page.style.cssText += "--white: #000000";
-		page.style.cssText += "--black: #ffffff";
-
-		// Replacing the stylesheets for code display
-		$("link[rel=stylesheet][href~='./highlight/styles/atom-one-light.css']").remove();
-		$("head").append("<link rel='stylesheet' href='highlight/styles/atom-one-dark.css'>");
-
-		localStorage["mode"] = "dark"; // Updating local storage
-
-		// Animation for changing icon once the moon has set
-		window.setTimeout(function(){
-			$("#moonIcon").removeClass("far fa-moon"); // Swapping moon icon
-			$("#moonIcon").addClass("fas fa-moon");
-		}, 200);
-	}
-	else
+	if(localStorage["mode"] === "dark") // If the site is in light mode, make it dark
 	{
 		page.style.cssText = "--mydark: #414345";
 		page.style.cssText += "--mylight: #f9f9f9";
@@ -52,6 +26,32 @@ function toggleNightMode()
 		window.setTimeout(function(){
 			$("#moonIcon").removeClass("fas fa-moon"); // Swapping moon icon
 			$("#moonIcon").addClass("far fa-moon");
+		}, 200);
+	}
+	else
+	{
+		page.style.cssText = "--mydark: #ced0d9";
+		page.style.cssText += "--mylight: #282c34";
+		page.style.cssText += "--mygray: #1f2329";
+		page.style.cssText += "--mylight-gray: #343b45"
+		page.style.cssText += "--mydark-gray: #21242b";
+		page.style.cssText += "--nav-hover: #3b3645";
+		page.style.cssText += "--nav-gradient-1: #33303d";
+		page.style.cssText += "--nav-gradient-2: #1c1e24";
+		page.style.cssText += "--display-light: #1a191f"
+		page.style.cssText += "--white: #000000";
+		page.style.cssText += "--black: #ffffff";
+
+		// Replacing the stylesheets for code display
+		$("link[rel=stylesheet][href~='./highlight/styles/atom-one-light.css']").remove();
+		$("head").append("<link rel='stylesheet' href='highlight/styles/atom-one-dark.css'>");
+
+		localStorage["mode"] = "dark"; // Updating local storage
+
+		// Animation for changing icon once the moon has set
+		window.setTimeout(function(){
+			$("#moonIcon").removeClass("far fa-moon"); // Swapping moon icon
+			$("#moonIcon").addClass("fas fa-moon");
 		}, 200);
 	}
 };
